c3419c1c5ff4b75ef65601f1ad9ff50dd85da8f8
Your Name <official.bipinchandra@gmail.com>
2025-12-06 22:07:40 +0530

    recursion practice

---
+++ 4 functions/Recursion_in_C/recursion_factorial.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+int fact(int n){
+    if(n==0){
+        return 1;
+    }
+    else{
+        return n*fact(n-1);
+    }
+}
+ void main() {
+    int num, result;
+   printf("factoria by recursion method \n");
+   printf("Enter the number ");
+   scanf("%d",&num);
+   result = fact(num);
+   printf("the factoria is %d",result);
+
+
+   getch();
+}
